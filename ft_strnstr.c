0978121b51f4fa4d0fefeb6865906acdd38da235
katsuyayoneno <hjkshn0405@gmail.com>
2023-01-25 13:56:17 +0900

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,31 +6,30 @@
 /*   By: kyoneno <hjkshn0405@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 23:01:04 by kyoneno           #+#    #+#             */
-/*   Updated: 2023/01/25 02:18:36 by kyoneno          ###   ########.fr       */
+/*   Updated: 2023/01/25 13:55:44 by kyoneno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	int			size_little;
-	const char	*end;
+	int			len_little;
+	size_t		i;
 
-	if (len == 0)
-		return (0);
-	if (!little)
+	i = 0;
+	len_little = ft_strlen((char *)little);
+	if (len_little == 0)
 		return ((char *)big);
-	size_little = ft_strlen((char *)little);
-	end = big + len;
-	while (big < end)
+	if (len_little > len)
+		return (0);
+	while (i < len - len_little)
 	{
-		if (*big == *little)
-		{
-			if (ft_memcmp(big, little, size_little) == 0)
-				return ((char *)big);
-		}
+		if (ft_strncmp(big, little, len_little) == 0)
+			return ((char *)big);
 		big++;
+		i++;
 	}
 	return (0);
 }
