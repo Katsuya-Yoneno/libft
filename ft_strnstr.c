47a955f1d339a8260a0acb590e94703b5112c418
katsuyayoneno <hjkshn0405@gmail.com>
2023-01-25 18:23:46 +0900

    ??

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,11 @@
 /*   By: kyoneno <hjkshn0405@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 23:01:04 by kyoneno           #+#    #+#             */
-/*   Updated: 2023/01/25 14:06:33 by kyoneno          ###   ########.fr       */
+/*   Updated: 2023/01/25 18:21:18 by kyoneno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
