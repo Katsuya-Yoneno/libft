4aed54dd80b5c39be44ca359c16649ae7f944f6c
katsuyayoneno <hjkshn0405@gmail.com>
2023-01-25 14:06:48 +0900

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: kyoneno <hjkshn0405@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 23:01:04 by kyoneno           #+#    #+#             */
-/*   Updated: 2023/01/25 13:55:44 by kyoneno          ###   ########.fr       */
+/*   Updated: 2023/01/25 14:06:33 by kyoneno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,20 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	int			len_little;
+	size_t			len_little;
 	size_t		i;
 
 	i = 0;
 	len_little = ft_strlen((char *)little);
+
+	// littleがnullだった場合にbigを返す
 	if (len_little == 0)
 		return ((char *)big);
+	// littleの長さが探す文字数よりも長い場合にnullを返す
 	if (len_little > len)
 		return (0);
-	while (i < len - len_little)
+	// 探す回数はlen-littleの文字数
+	while (i <= len - len_little)
 	{
 		if (ft_strncmp(big, little, len_little) == 0)
 			return ((char *)big);
