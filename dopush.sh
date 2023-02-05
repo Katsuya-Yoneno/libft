6cec6ec0c7c1064854338b6d455ad80404c469a2
katsuyayoneno <hjkshn0405@gmail.com>
2023-02-05 15:01:46 +0900

    libft

--- dopush.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git add .
-git commit -m "from dopush.sh"
-git push origine master
--- ft_atoi.c
+++ ft_atoi.c
@@ -6,17 +6,28 @@
 /*   By: kyoneno <hjkshn0405@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 23:03:51 by kyoneno           #+#    #+#             */
-/*   Updated: 2023/02/05 13:19:38 by kyoneno          ###   ########.fr       */
+/*   Updated: 2023/02/05 15:00:57 by kyoneno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "libft.h"
 
-int ft_isOverflow(const char *nptr, int isNegative, long int num, int i)
+int	ft_isNegative(const char *nptr, int i)
 {
-	long int ov_div;
-	int      ov_mod;
+	int isNegative;
+	isNegative = 0;
+	while (nptr[i] == '+' || nptr[i] == '-' )
+	{
+		if (nptr[i] == '-')
+			isNegative = 1;
+	}
+	return (isNegative);
+}
+
+int	ft_isOverflow(const char *nptr, int isNegative, long int num, int i)
+{
+	long int	ov_div;
+	int			ov_mod;
 
 	ov_div = LONG_MAX / 10;
 	ov_mod = LONG_MAX % 10;
@@ -33,10 +44,10 @@ int ft_isOverflow(const char *nptr, int isNegative, long int num, int i)
 	return (0);
 }
 
-int ft_isUnderflow(const char *nptr, int isNegative, long int num, int i)
+int	ft_isUnderflow(const char *nptr, int isNegative, long int num, int i)
 {
-	long int ov_div;
-	int      ov_mod;
+	long int	ov_div;
+	int			ov_mod;
 
 	ov_div = (-1) * (LONG_MIN / 10);
 	ov_mod = (-1) * (LONG_MIN % 10);
@@ -53,50 +64,44 @@ int ft_isUnderflow(const char *nptr, int isNegative, long int num, int i)
 	return (0);
 }
 
-int	ft_atoi(const char *nptr)
+long	ft_add_num(const char *nptr, long int num, int i, int isNegative)
 {
-	long int	num;
-	int	        isNegative;
-	int i;
-	long int ov_div;
+	long int	ov_div;
 
-	num = 0;
-	i = 0;
-	isNegative = 0;
-	ov_div = LONG_MAX / 10;
-	//isspace関数の既定に沿った空白文字
-	while (('\t' <= nptr[i] && nptr[i] <= '\r') || nptr[i] == ' ')
-	{
-		i++;
-	}
-	while (nptr[i] == '+' || nptr[i] == '-' )
-	{
-		// 負の数なら、'-'カウンタを増やしてポインタを進める
-		if (nptr[i] == '-')
-			isNegative = 1;
-		i++;
-	}
+	ov_div = LONG_MAX / 10;	
 	while ('0' <= nptr[i] && nptr[i] <= '9' && nptr[i])
 	{
 		num = num * 10 + (nptr[i] - '0');
-		if (ft_isOverflow(nptr, isNegative, num, i))
-		{
-			printf("%s\n", "overflow");
-			return ((int)LONG_MAX);
-		}
-		if (ft_isUnderflow(nptr, isNegative, num, i))
-		{
-			printf("%s\n", "underflow");
-			return ((int)LONG_MIN);
-		}
 		if (num == ov_div)
 		{
 			i++;
 			num = num * 10 + (nptr[i] - '0');
-			break;
+			return (num);
 		}
+		if (ft_isOverflow(nptr, isNegative, num, i))
+			return (LONG_MAX);
+		if (ft_isUnderflow(nptr, isNegative, num, i))
+			return (LONG_MIN);
 		i++;
 	}
+	return (num);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	long int	num;
+	int			isNegative;
+	int			i;
+
+	num = 0;
+	i = 0;
+	isNegative = 0;
+	while (('\t' <= nptr[i] && nptr[i] <= '\r') || nptr[i] == ' ')
+		i++;
+	isNegative = ft_isNegative(nptr, i);
+	if (isNegative)
+		i++;
+	num = ft_add_num(nptr, num, i, isNegative);
 	if (isNegative)
 		num = 0 - num;
 	return ((int)num);
